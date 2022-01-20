31cd953bc7bc32ea7fa80142f04d178486b53ae1
Niels <niels_martens@live.nl>
2022-01-20 12:23:31 +0100

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { appModel } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kindfox';
+export class AppComponent implements OnInit {
+  
+  daycares$!: appModel[]
+
+  constructor(private appService: AppService){}
+
+  ngOnInit() {
+      this.appService.getDaycares().subscribe(daycare => this.daycares$ = daycare)
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { appModel } from './app.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  url: string = "http://localhost/daycare/public/api"
+
+  constructor(private http: HttpClient) { }
+
+  getDaycares(): Observable<any> {
+    return this.http.get<[appModel[]]>(this.url).pipe();
+  }
+
+}
