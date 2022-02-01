8807fa9ffc172c232d272995286cf455e8a88290
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-02-01 13:25:54 +0100

    registerparentNotYetToServer

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Parent } from 'src/app/shared/model/parent.model'
 
 @Component({
   selector: 'app-register',
@@ -6,12 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  @Output() onSubmitted = new EventEmitter <Parent>();
+  
   isdaycare = false;
   
+  newParent!:Parent;
+  id:any = null;
+  firstname!: string;
+  lastname!: string;
+  email!: string;
+  phone!: number;
+  password!: string;
+  pasverify!: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  Onsubmit(){
 
+    if(this.isdaycare){
+
+    }
+    else{
+      if (this.pasverify == this.password){
+        const newParent = new Parent(
+          this.id, this.firstname, this.lastname,this.email,this.phone, this.password
+        )
+        console.log(newParent)
+      }
+      else{
+        window.alert("Incorrect password")
+      }
+    }
+
+  }
 }
