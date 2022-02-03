3a38e38e9d879511141a51ecfe39173b206c79fb
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-02-03 16:00:47 +0100

    loginattempt

--- src/app/loginscreen/loginscreen.component.ts
+++ src/app/loginscreen/loginscreen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatastorageService } from 'src/app/datastorage.service';
+import { Parent } from 'src/app/shared/model/parent.model';
 import { Routes } from '@angular/router';
 
 @Component({
@@ -8,19 +9,40 @@ import { Routes } from '@angular/router';
   styleUrls: ['./loginscreen.component.css']
 })
 export class LoginscreenComponent implements OnInit {
+  parents$!:Parent[];
+  email!:string;
+  password!:string;
 
-  constructor() { 
+  constructor(private dataStorage: DatastorageService) { 
     
   }
 
   ngOnInit(): void {
+    this.dataStorage.getAllParents().subscribe(parents => this.parents$ = parents)
   }
 
   Plogin(){
-
+    console.log(this.parents$);
+    /*
+    fetch(this.testurl + '/search/' + this.email).then(res=>res.json()).then(
+      res=> {
+        if(res.length == 0){
+          alert("wrong username or password")
+        }
+        else{
+          if(res[0].password == this.password){
+            alert("login succesfull");
+          }
+          else{
+            alert("wrong username or password")
+          }
+        }
+      }
+    )
+    */
   }
   DClogin(){
-    
+
   }
 
   registerP(){
