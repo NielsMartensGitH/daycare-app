72693e4478bb4986edde0ab842900cc84e6a4c44
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-02-15 13:50:53 +0100

    diaryswitch ugly fix

--- src/app/messageboard/mb-diaries/mb-diaries.component.ts
+++ src/app/messageboard/mb-diaries/mb-diaries.component.ts
@@ -1,6 +1,12 @@
+import { Injectable, ɵdefaultKeyValueDiffers } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { DatastorageService } from 'src/app/datastorage.service';
 import { TimeService } from 'src/app/time.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-mb-diaries',
@@ -15,15 +21,19 @@ export class MbDiariesComponent implements OnInit {
   childId!: any;
 
 
-  constructor(private dataStorage: DatastorageService, private timeService: TimeService) { }
+  constructor(private dataStorage: DatastorageService, private timeService: TimeService, private router:Router) { }
 
   ngOnInit(){
-    this.childId = sessionStorage.getItem('ChildId');
-
-    console.log(this.childId)
+    
+    if(this.childId == undefined){
+      this.childId = sessionStorage.getItem('ChildId');
+      console.log('defined it')
+    }
+    console.log(this.childId + ' im doing it')
     
     this.dataStorage.getChildDiaries(this.childId).subscribe( 
       diaries => {
+        console.log("hey uh het zit erin " + this.childId)
         this.diaries$ = diaries;
         this.diaries$.map((obj:any) => {
           obj.poop = obj.poop.split("&")
@@ -53,13 +63,25 @@ export class MbDiariesComponent implements OnInit {
 
  
  messageId(id: number) {
-  if (id == this.msgId) { // When we already opened the comments of this posts (when msgID is already known) it will close again
-    this.msgToggle = false;
-    this.msgId = 0;
-  } else { // else comments will be shown
-    this.msgId = id;
-    this.msgToggle = true;
-  }
+    if (id == this.msgId) { // When we already opened the comments of this posts (when msgID is already known) it will close again
+      this.msgToggle = false;
+      this.msgId = 0;
+    } else { // else comments will be shown
+      this.msgId = id;
+      this.msgToggle = true;
+    }
  
-}
+  }
+  setchild(newchildid:any){
+    this.childId = newchildid;
+    console.log(this.childId);
+    //this.ngOnInit();
+    this.reloadComponent();
+  }
+  reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
+  }
 }
--- src/app/messageboard/mb_sidebar/mb-addchildren/mb-addchildren.component.ts
+++ src/app/messageboard/mb_sidebar/mb-addchildren/mb-addchildren.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DatastorageService } from 'src/app/datastorage.service';
 import { Child } from 'src/app/shared/model/child.models';
+import { MbDiariesComponent } from'src/app/messageboard/mb-diaries/mb-diaries.component';
 
 @Component({
   selector: 'app-mb-addchildren',
@@ -12,7 +13,7 @@ export class MbAddchildrenComponent implements OnInit {
   parent_id = sessionStorage.getItem('parentID')
   childparents$!:Child[];
   curchild!: any;
-  constructor(private dataStorage:DatastorageService) { }
+  constructor(private dataStorage:DatastorageService, private changechild: MbDiariesComponent) { }
 
   ngOnInit(): void {
     this.dataStorage.getChildParents(this.parent_id).subscribe(childparents => {this.childparents$ = childparents
@@ -26,7 +27,8 @@ export class MbAddchildrenComponent implements OnInit {
 
   addChildId(child: any) {
     sessionStorage.setItem('ChildId', child);
-
+    this.changechild.setchild(child);
+    console.log("send" + child)
   }
 
   // showInfochild(selectedchild:any){
